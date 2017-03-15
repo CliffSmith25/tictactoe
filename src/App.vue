aa7771022abf8901667976fba51899e18ed78c2e
Cliff <cliffsmith25@gmail.com>
2017-03-14 20:13:23 -0500

    Working App built to deploy

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,118 @@
 <template>
   <div id="app">
+    <h1>{{ msg }}</h1>
     <table id="board">
       <tr>
-        <td id="0" v-bind:class="{ active: isActive[0] }" v-on:click="played(0)">{{ tableState[0] }}</td>
-        <td id="1" v-bind:class="{ active: isActive[1] }" v-on:click="played(1)">{{ tableState[1] }}</td>
-        <td id="2" v-bind:class="{ active: isActive[2] }" v-on:click="played(2)">{{ tableState[2] }}</td>
+        <td id="0" v-bind:class="{ inactive: isInactive[0] }" v-on:click="playerPlayed(0)">{{ tableState[0] }}</td>
+        <td id="1" v-bind:class="{ inactive: isInactive[1] }" v-on:click="playerPlayed(1)">{{ tableState[1] }}</td>
+        <td id="2" v-bind:class="{ inactive: isInactive[2] }" v-on:click="playerPlayed(2)">{{ tableState[2] }}</td>
       </tr>
       <tr>
-        <td id="3" v-bind:class="{ active: isActive[3] }" v-on:click="played(3)">{{ tableState[3] }}</td>
-        <td id="4" v-bind:class="{ active: isActive[4] }" v-on:click="played(4)">{{ tableState[4] }}</td>
-        <td id="5" v-bind:class="{ active: isActive[5] }" v-on:click="played(5)">{{ tableState[5] }}</td>
+        <td id="3" v-bind:class="{ inactive: isInactive[3] }" v-on:click="playerPlayed(3)">{{ tableState[3] }}</td>
+        <td id="4" v-bind:class="{ inactive: isInactive[4] }" v-on:click="playerPlayed(4)">{{ tableState[4] }}</td>
+        <td id="5" v-bind:class="{ inactive: isInactive[5] }" v-on:click="playerPlayed(5)">{{ tableState[5] }}</td>
       </tr>
       <tr>
-        <td id="6" v-bind:class="{ active: isActive[6] }" v-on:click="played(6)">{{ tableState[6] }}</td>
-        <td id="7" v-bind:class="{ active: isActive[7] }" v-on:click="played(7)">{{ tableState[7] }}</td>
-        <td id="8" v-bind:class="{ active: isActive[8] }" v-on:click="played(8)">{{ tableState[8] }}</td>
+        <td id="6" v-bind:class="{ inactive: isInactive[6] }" v-on:click="playerPlayed(6)">{{ tableState[6] }}</td>
+        <td id="7" v-bind:class="{ inactive: isInactive[7] }" v-on:click="playerPlayed(7)">{{ tableState[7] }}</td>
+        <td id="8" v-bind:class="{ inactive: isInactive[8] }" v-on:click="playerPlayed(8)">{{ tableState[8] }}</td>
       </tr>
     </table>
+    <div id="playerDiv">
+      <h2>Select X or O</h2>
+      <div id="radio">
+        <input type="radio" name="XO" value="X" v-model="playerPiece"><label>X</label>
+        <input type="radio" name="XO" value="O" v-model="playerPiece"><label>O</label>
+      </div>
+    </div>
+    <h4><a href="https://github.com/CliffSmith25/tictactoe">View the source code here</a></h4>
   </div>
 </template>
 
 <script>
-
-import { checkForWinner } from './modules/ai.js'
+import { checkForWinner, computerPlay } from './modules/ai.js'
 
 const initState = ['', '', '', '', '', '', '', '', '']
-const initActive = [true, true, true, true, true, true, true, true, true]
+const initInactive = [false, false, false, false, false, false, false, false, false]
 
 export default {
   name: 'app',
   data () {
     return {
       tableState: initState.slice(),
-      turn: 'O',
-      isActive: initActive.slice(),
-      compTurn: false
+      isInactive: initInactive.slice(),
+      compTurn: false,
+      playerPiece: 'X',
+      msg: this.initGame()
+    }
+  },
+  watch: {
+    playerPiece: function () {
+      this.initGame()
+    }
+  },
+  computed: {
+    compPiece: function () {
+      if (this.playerPiece === 'X') return 'O'
+      else return 'X'
+    },
+    turn: function () {
+      if (this.compTurn) return this.compPiece
+      else return this.playerPiece
     }
   },
   methods: {
     toggleTurn: function () {
-      if (this.turn === 'O') this.turn = 'X'
-      else this.turn = 'O'
+      this.compTurn = !this.compTurn
+      this.msg = (this.compTurn) ? 'Computer Playing' : 'Your Turn'
     },
-    played: function (x) {
+    playerPlayed: function (x) {
       let tempTable = this.tableState.slice()
       tempTable[x] = this.turn
       this.tableState = tempTable
-      let tempActive = this.isActive.slice()
-      tempActive[x] = false
-      this.isActive = tempActive
-      this.checkWin()
-      this.toggleTurn()
+      let tempInactive = this.isInactive.slice()
+      tempInactive[x] = true
+      this.isInactive = tempInactive
+      if (checkForWinner(this.tableState) !== 'no winner') {
+        this.msg = 'You win!'
+        setTimeout(this.initGame, 2000)
+      } else if (this.tableState.join('').length === 9) {
+        this.msg = 'Game over - Tie!'
+        setTimeout(this.initGame, 2000)
+      } else {
+        this.toggleTurn()
+        this.playComputerTurn()
+      }
     },
-    checkWin: function () {
-      const didWin = checkForWinner(this.tableState)
-      console.log(didWin)
+    playComputerTurn: function () {
+      const i = computerPlay(this.tableState, 'X')
+      this.tableState[i] = this.compPiece
+      let tempInactive = this.isInactive.slice()
+      tempInactive[i] = true
+      this.isInactive = tempInactive
+      if (checkForWinner(this.tableState) !== 'no winner') {
+        this.msg = 'Computer Wins!'
+        setTimeout(this.initGame, 2000)
+      } else if (this.tableState.join('').length === 9) {
+        this.msg = 'Game over - Tie!'
+        setTimeout(this.initGame, 2000)
+      } else {
+        this.toggleTurn()
+      }
+    },
+    getFirstPlayer: function () {
+      return (Math.random() >= 0.5)
+    },
+    initGame: function () {
+      this.tableState = initState.slice()
+      this.isInactive = initInactive.slice()
+      this.compTurn = this.getFirstPlayer()
+      this.msg = (this.compTurn) ? 'Computer Playing' : 'Your Turn'
+      if (this.compTurn) this.playComputerTurn()
+      return this.msg
     }
   }
 }
-
 </script>
 
 <style>
@@ -71,10 +125,23 @@ export default {
   margin-top: 60px;
 }
 
+#radio {
+  display: inline-flex;
+  align-items: center;
+}
+
+input {
+  margin-left: 2em;
+}
+
+label {
+  font-size: 2em;
+}
+
 td {
   width: 33%;
   height: 33%;
-  pointer-events: none;
+  cursor: pointer;
 }
 
 td + td {
@@ -87,8 +154,8 @@ tr + tr {
   border-color: #ccdfcb;
 }
 
-.active {
-  pointer-events: auto;
+.inactive {
+  pointer-events: none;
 }
 
 table {
--- src/modules/ai.js
+++ src/modules/ai.js
@@ -1,14 +1,58 @@
 
-export function checkForWinner (boardState) {
-  const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+const winCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-  return winCombos.map(arr => {
-    return arr.reduce((acc, num) => {
-      return boardState[num] + acc
-    }, '')
-  }).reduce((acc, str) => {
+export function checkForWinner (boardState) {
+  return mapWinCombos(boardState).reduce((acc, str) => {
     if (str === 'XXX') return 'X Wins!'
     else if (str === 'OOO') return 'O Wins!'
     else return acc
   }, 'no winner')
+}
+
+export function computerPlay (boardState, char) {
+  const compCanWin = canWin(boardState, char)
+  const humanCanWin = canWin(boardState, 'O')
+  if (compCanWin.canWin === true) return getWinSquare(winCombos[compCanWin.combo], boardState)
+  else if (humanCanWin.canWin === true) return getWinSquare(winCombos[humanCanWin.combo], boardState)
+  else if (boardState[4] === '') return 4
+  else if (cornerAvailable(boardState) !== false) {
+    return cornerAvailable(boardState)
+  } else return nextAvailable(boardState)
+}
+
+function nextAvailable (boardState) {
+  for (let i = 0; i < boardState.length; i++) {
+    if (boardState[i] === '') return i
+  }
+}
+
+function cornerAvailable (boardState) {
+  const corners = [0, 2, 6, 8]
+  return corners.reduce((acc, val) => {
+    return (boardState[val] === '') ? val : acc
+  }, false)
+}
+
+function getWinSquare (winRow, boardState) {
+  for (let i = 0; i < 3; i++) {
+    if (boardState[winRow[i]] === '') {
+      return winRow[i]
+    }
+  }
+}
+
+function mapWinCombos (boardState, char) {
+  return winCombos.map(arr => {
+    return arr.reduce((acc, num) => {
+      return boardState[num] + acc
+    }, '')
+  })
+}
+
+function canWin (boardState, char) {
+  const canWinSpots = mapWinCombos(boardState)
+  for (let i = 0; i < canWinSpots.length; i++) {
+    if (canWinSpots[i] === char + char) return { canWin: true, combo: i }
+  }
+  return { canWin: false }
 }
