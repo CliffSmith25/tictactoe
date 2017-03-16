035893adb5b54ea7b65752e0c75e0489a41d3f29
Cliff <cliffsmith25@gmail.com>
2017-03-16 17:41:24 -0500

    Added function for determining score

--- src/modules/ai.js
+++ src/modules/ai.js
@@ -14,10 +14,87 @@ export function computerPlay (boardState, compChar, humChar) {
   const humanCanWin = canWin(boardState, humChar)
   if (compCanWin.canWin === true) return getWinSquare(winCombos[compCanWin.combo], boardState)
   else if (humanCanWin.canWin === true) return getWinSquare(winCombos[humanCanWin.combo], boardState)
+  else if (boardState.join('').length === 1) return getSecondMove(boardState)
   else if (boardState[4] === '') return 4
   else if (cornerAvailable(boardState) !== false) {
     return cornerAvailable(boardState)
   } else return nextAvailable(boardState)
+}
+
+/*
+function minimax (board, level, player, compChar, humChar) {
+  if (gameover || level === 0) return getScore(board, compChar, humChar)
+  let children = getLegalMoves(board)
+  if (player === 'computer') {
+    let bestScore = Number.NEGATIVE_INFINITY
+    for (let i = 0; i < children.length; i++) {
+      let score = minimax(level - 1, 'opponent')
+      if (score > bestScore) bestScore = score
+    }
+    return bestScore
+  } else {
+    let bestScore = Number.POSITIVE_INFINITY
+    for (let i = 0; i < children.length; i++) {
+      let score = minimax(level - 1, 'computer')
+      if (score < bestScore) bestScore = score
+    }
+    return bestScore
+  }
+}
+*/
+
+console.log(getScore(['X', 'X', 'O', '', '', 'O', '', '', 'O'], 'X', 'O'))
+console.log(getLegalMoves(['X', 'O', '', '', 'X', 'O', '', '', '']))
+
+function getScore (board, compChar, humChar) {
+  let score = 0
+  winCombos.map(winRow => {
+    let row = winRow.map(y => {
+      return board[y]
+    }).join('')
+    switch (row) {
+      case compChar.repeat(3):
+        score += 100
+        break
+      case humChar.repeat(3):
+        score -= 100
+        break
+      case compChar.repeat(2):
+        score += 10
+        break
+      case humChar.repeat(2):
+        score -= 10
+        break
+      case compChar:
+        score += 1
+        break
+      case humChar:
+        score -= 1
+        break
+    }
+  })
+  return score
+}
+
+function getLegalMoves (board) {
+  let arr = []
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === '') arr.push(i)
+  }
+  return arr
+}
+
+function getSecondMove (boardState) {
+  if (boardState[4] !== '') return 0
+  else {
+    const corners = [[0, 8], [2, 6]]
+    return corners.reduce((acc, val) => {
+      console.log(boardState[val[1]])
+      if (boardState[val[0]] !== '') return val[1]
+      else if (boardState[val[1]] !== '') return val[0]
+      else return acc
+    }, 0)
+  }
 }
 
 function nextAvailable (boardState) {
